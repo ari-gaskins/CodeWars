263adcb6a251a2e1aa21f1ea495385bb80877eda
Ari Gaskins <arigaskins.tech@gmail.com>
2022-04-15 15:19:34 -0400

    multiplicative persistence

---
+++ persistent_bugger.py
@@ -0,0 +1,16 @@
+def persistence(n):
+    if str(n) == 1:
+        return 0
+    count = 0
+    while len(str(n)) > 1:
+        num = 1
+        for i in str(n):
+            num *= int(i)
+        n = num
+        count += 1
+    print(count) 
+
+persistence(39)
+
+
+
