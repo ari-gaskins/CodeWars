41bab98221e9ab6800d9060edbfd500f30083536
Ari Gaskins <arigaskins.tech@gmail.com>
2022-04-28 14:27:12 -0400

    change n formula

--- sum_odd_num.py
+++ sum_odd_num.py
@@ -1,6 +1,6 @@
 def row_sum_odd_numbers(n):
     n = int(n)
-    term = int(n * (n + 2)/2)
+    term = int(n * (n + 1)/2)
     print('The ' + str(n) + 'th term is ' + str(term))
 
 num = input('Enter n value: ')
