fa21bdef55018e006754739d77a3f54978025e1d
Ari Gaskins <arigaskins.tech@gmail.com>
2022-04-28 14:53:56 -0400

    change triangular formula

--- sum_odd_num.py
+++ sum_odd_num.py
@@ -2,7 +2,7 @@ def row_sum_odd_numbers(n):
     n = int(n)
     for i in range(n, 7):
         n = i
-        term = int(n * (n + 1)/2)
+        term = int(n * (n + 3)/2)
         print('The ' + str(n) + 'th term is ' + str(term))
 
 num = input('Enter n value: ')
