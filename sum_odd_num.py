1e50719256b020e01d2cbd9405e926cbfcb436c8
Ari Gaskins <arigaskins.tech@gmail.com>
2022-04-28 15:16:39 -0400

    testing digits

--- sum_odd_num.py
+++ sum_odd_num.py
@@ -1,9 +1,12 @@
 def row_sum_odd_numbers(n):
     n = int(n)
+    digit = 1
     for i in range(n, 7):
         n = i
         term = int(n * (n + 1)/2)
-        print('The ' + str(n) + 'th term is ' + str(term))
+        digit +=2
+        print(f'For the {str(n)}th term of {str(term)} the digit is {str(digit)}')
+        
 
 num = input('Enter n value: ')
 
