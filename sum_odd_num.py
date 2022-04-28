ad3342c5d2f1e7b00875e08424ab3269ee397c16
Ari Gaskins <arigaskins.tech@gmail.com>
2022-04-28 14:52:23 -0400

    change range

--- sum_odd_num.py
+++ sum_odd_num.py
@@ -1,6 +1,6 @@
 def row_sum_odd_numbers(n):
     n = int(n)
-    for i in range(n, 6):
+    for i in range(n, 7):
         n = i
         term = int(n * (n + 1)/2)
         print('The ' + str(n) + 'th term is ' + str(term))
