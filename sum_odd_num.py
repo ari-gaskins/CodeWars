07a27c8969c3c614734f19da19cc7d7b87dc2452
Ari Gaskins <arigaskins.tech@gmail.com>
2022-04-28 14:34:35 -0400

    attempted iterable

--- sum_odd_num.py
+++ sum_odd_num.py
@@ -1,7 +1,9 @@
 def row_sum_odd_numbers(n):
     n = int(n)
-    term = int(n * (n + 1)/2)
-    print('The ' + str(n) + 'th term is ' + str(term))
+    for i in range(n, 6):
+        n = i
+        term = int(n * (n + 1)/2)
+        print('The ' + str(n) + 'th term is ' + str(term))
 
 num = input('Enter n value: ')
 
