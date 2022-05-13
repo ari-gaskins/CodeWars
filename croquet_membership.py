8cc7fbaeef200a32150bb087117dbb4fe43cb39a
Ari Gaskins <arigaskins.tech@gmail.com>
2022-05-12 20:55:31 -0400

    commit out test, add process to primary function

--- croquet_membership.py
+++ croquet_membership.py
@@ -3,6 +3,7 @@
 # open is everyone not categorized as a senior
 
 def open_or_senior(data):
+    members = []
     age = data[:1]
     for i in range(len(age)):
         age = age[i]
@@ -10,28 +11,29 @@ def open_or_senior(data):
     for i in range(len(handicap)):
         handicap = handicap[i]
     if age >= 55 and handicap > 7:
-        print('senior')
+        members.append('senior')
     else:
-        print('open')
+        members.append('open')
+    return members
 
 
 open_or_senior([18, 20])
 open_or_senior([45, 2])
 open_or_senior([61, 12])
 
-def list_of_strings(answer):
-    list = []
-    yes = 'y'
-    no = 'n'
-    if answer is yes:
-        list.append(yes)
-    else:
-        list.append(no)
-    print(list)
+#def list_of_strings(answer):
+#    list = []
+#    yes = 'y'
+#    no = 'n'
+#    if answer is yes:
+#        list.append(yes)
+#    else:
+#        list.append(no)
+#    print(list)
 
-list_of_strings('y')
-list_of_strings('n')
-list_of_strings('n')
+#list_of_strings('y')
+#list_of_strings('n')
+#list_of_strings('n')
 
 #def sliced_list(data):
 #    age = data[:1]
