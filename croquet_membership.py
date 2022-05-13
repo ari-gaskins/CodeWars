f52a21d14f25d2f969cfbc6f8731cbec51dba0f5
Ari Gaskins <arigaskins.tech@gmail.com>
2022-05-12 20:40:56 -0400

    forgot to add problem, working with list appending

---
+++ croquet_membership.py
@@ -0,0 +1,43 @@
+# categorize members into 'senior' or 'open'
+# senior is age 55+ with a handicap > 7
+# open is everyone not categorized as a senior
+
+def open_or_senior(data):
+    age = data[:1]
+    for i in range(len(age)):
+        age = age[i]
+    handicap = data[1:]
+    for i in range(len(handicap)):
+        handicap = handicap[i]
+    if age >= 55 and handicap > 7:
+        print('senior')
+    else:
+        print('open')
+
+
+open_or_senior([18, 20])
+open_or_senior([45, 2])
+open_or_senior([61, 12])
+
+def list_of_strings(answer):
+    list = []
+    yes = 'y'
+    no = 'n'
+    if answer is yes:
+        list.append(yes)
+    else:
+        list.append(no)
+
+list_of_strings('y')
+list_of_strings('n')
+list_of_strings('n')
+
+#def sliced_list(data):
+#    age = data[:1]
+#    handicap = data[1:]
+#    print(age)
+#    print(handicap)
+
+
+#sliced_list([18, 20])
+#sliced_list([45, 2])
