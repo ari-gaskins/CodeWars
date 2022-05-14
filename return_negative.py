f0c37b7148a5d49da8bf396db3c64f0ece7a735a
Ari Gaskins <arigaskins.tech@gmail.com>
2022-05-14 14:51:02 -0400

    function to test if negative

--- return_negative.py
+++ return_negative.py
@@ -2,3 +2,10 @@
 # if the number is already negative, return that number unchanged
 # if the number is zero, return zero unchanged
 
+def make_negative(number):
+    if number <= 0:
+        return number
+
+make_negative(0)
+make_negative(-1)
+make_negative(-2)
