c8180735957f3b684456912549485a6784e7e1df
Ari Gaskins <arigaskins.tech@gmail.com>
2022-05-14 15:13:46 -0400

    create function to convert string into list

--- two_to_one.py
+++ two_to_one.py
@@ -2,3 +2,17 @@
 # return a string with a combination of all letters 
 # with only one of each distinct letter
 # and sorted alphabetically
+
+def longest(s1, s2):
+    return s1 + s2
+
+
+def string_to_list(s1):
+    str_list = list(s1.split(''))
+    print(str_list)
+
+a = "xyaabbbccccdefww"
+b = "xxxxyyyyabklmopq"
+
+string_to_list(a)
+string_to_list(b)
